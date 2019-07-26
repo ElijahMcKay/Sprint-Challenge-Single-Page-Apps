e137b85b2b611893a2bb7e2dbe0d4179f47368d5
ElijahMcKay <45011056+ElijahMcKay@users.noreply.github.com>
2019-07-26 10:29:20 -0500

    doing api call setup for Characters page

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,11 +1,21 @@
 import React, { useEffect, useState } from 'react';
+import axios from "axios"; 
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
+  const [data, setData] = useState([]); 
 
   useEffect(() => {
     // TODO: Add AJAX/API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+
+    axios.get('https://rickandmortyapi.com/documentation/#get-all-characters')
+    .then(response => {
+      console.log('Success', response)
+    })
+    .catch(error => {
+      console.log('Something went wrong:', error); 
+    })
   }, [])
 
   return <section className='character-list grid-view'>
