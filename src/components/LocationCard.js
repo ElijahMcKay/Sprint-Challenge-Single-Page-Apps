db4ee2d57463e98175c4b2cc5bc76f1a9c5522c9
ElijahMcKay <45011056+ElijahMcKay@users.noreply.github.com>
2019-07-26 12:17:36 -0500

    working on semantic ui styling

--- src/components/LocationCard.js
+++ src/components/LocationCard.js
@@ -1,13 +1,30 @@
 import React from 'react'
+import { Card, Icon, Image } from 'semantic-ui-react'
 
 export default function LocationCard (props) {
   // image={image}
   return (
-    <div> 
-      <h2>{props.name}</h2>
-      <h3>{props.type}</h3>
-      <p>{props.dimension}</p>
-      <p>{props.residents.length}</p>
-    </div> 
+    // <div> 
+    //   <h2>{props.name}</h2>
+    //   <h3>{props.type}</h3>
+    //   <p>{props.dimension}</p>
+    //   <p>{props.residents.length}</p>
+    // </div> 
+    <Card>
+      {/* <Image src={props.pfp} wrapped ui={false} /> */}
+      <Card.Content>
+        <Card.Header>{props.name}</Card.Header>
+        <Card.Meta>{props.type}</Card.Meta>
+        <Card.Description>
+          {props.dimension}
+        </Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        <a>
+          <Icon name='user' />
+          {`Residents: ${props.residents.length}`}
+        </a>
+      </Card.Content>
+    </Card>
   )
 }
